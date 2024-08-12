888f34e437280100f174daf0080a8d130a6dbe45
Rajat Singh <126982984+Rajat3254@users.noreply.github.com>
2024-08-12 16:22:37 +0000

    day : 2

---
+++ basics/dataType.js
@@ -0,0 +1,9 @@
+// number ki range 2 to the power 53 to -2 to the power 53
+// String prefer to use ""
+// null is a stand alone value
+// null and undefined are different to assgin null we delibrately do it 
+// ex: let n=null but in undefined we do ex:let n; so the n is undefined as of now
+// boolean true/false
+// to get the type of the datatype we use typeof
+//ex: console.log(typeof null) object
+// console.log(type of undefined) undefined
