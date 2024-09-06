289547bef7800d7d827a256658c66ff86a51e64b
Rajat Singh <126982984+Rajat3254@users.noreply.github.com>
2024-09-06 17:33:17 +0000

    Day : 09

---
+++ basics_2/How_does_javascript_code_works.js
@@ -0,0 +1,5 @@
+// hota kya hai ki hamara code likhne ke bad 
+// 1. global execution hota hai jo ki this naam ke keyword me store ho jata hai 
+// 2. global execution ke baad memory execution hot hai isme jitne bhi variable hote hai aur function ,object hote hai unka name and defination sttore ho jata hai
+// 3. execution phase ke andar jo values hoti hai woh variables ko mitli hai aur execution hota hai
+// 4. jitne baar function call hota hai utni baar ye steps repeat hote hai unke liye separetaly. 
