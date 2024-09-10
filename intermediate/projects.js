edc653a64b67d5c80dcddb23d7bbc492b080dc6c
Rajat Singh <126982984+Rajat3254@users.noreply.github.com>
2024-09-10 18:19:22 +0000

    Day : 13

---
+++ intermediate/projects.js
@@ -0,0 +1,6 @@
+// hamne banaya bg color switcher 
+// hamne eventListner laga diye each block par using for each loop 
+// buttons.addEventListner('click',function (e){  // name nhi dete hai function ka
+// if(e.target.id==='red'){ // e ke andar bahut kuch hota hai jese target aur uske andar saare attributes hote hai
+//   body.style.backgroundcolor=e.target.id;
+// }})
