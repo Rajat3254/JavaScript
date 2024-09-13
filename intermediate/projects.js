9826ac3b95a81a94fd605c06eb03ac23f3bc939f
Rajat Singh <126982984+Rajat3254@users.noreply.github.com>
2024-09-13 17:36:24 +0000

    Day : 14

--- intermediate/projects.js
+++ intermediate/projects.js
@@ -4,3 +4,13 @@
 // if(e.target.id==='red'){ // e ke andar bahut kuch hota hai jese target aur uske andar saare attributes hote hai
 //   body.style.backgroundcolor=e.target.id;
 // }})
+
+
+// 2. Digital clock isme ham ko ek element ke ander time dikhana hai jo change ho each second pe iske liye ham use karte hai setIntervalfunction ko
+// woh ek function aur kitne time baad value change hoga woh leta hai
+// let date=new date()
+// // ye to object hai isko banake hi time extract karenge
+// setInterval(function(){
+//     elementName.innerText=date.toLocaleTimeString();
+//     // iske help se jo date aayega uska time bas dikhega
+// },1000)
