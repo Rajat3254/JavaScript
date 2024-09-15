6f7cbbeb4cca6d607b5739366d6116a22ffbf865
Rajat Singh <126982984+Rajat3254@users.noreply.github.com>
2024-09-15 18:11:52 +0000

    Day : 16

---
+++ intermediate/async.js
@@ -0,0 +1,5 @@
+// javaScript ek synchronus laguage hai means ek ke bad ek line of code eexecute hota hai aur woh single threaded bhi hai means ek thread pe hi sab kuch hota hai
+// ye default javascript hai
+
+// sync hota hai tab jab ham blocking code likhte hai means ek min ruko aur aata hu lekin us time tak koi kaam nhi hoga
+// async hota hai jab ham non blocking code likhte hai means ek min ruko aur aata hu lekin us time tak koi aur kaam kar sakte hai ham
