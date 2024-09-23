729646003666d43ba8811db39a2c280d7417bc88
Rajat Singh <126982984+Rajat3254@users.noreply.github.com>
2024-09-23 17:56:37 +0000

    Day : 18

--- intermediate/setTimeOut.js
+++ intermediate/setTimeOut.js
@@ -2,3 +2,16 @@
 setTimeout(function(){
 console.log("rajat");
 },2000)
+
+// Agar hame kisi timeout function ko rokana hai to ham clearTimeOut() ka use karte hai ab timeout function dher saare ho sakte hai
+// to jiko rokna hai usko ek variable me store karlo 
+// ex. const rukja=setTimeout(function(){
+// },2000);
+// lekin if ham timeout ke andar nahi roke to woh nhi rukega lekin direct likhne par ruk hi jata hai ab lekin  agar kisi button pe
+// eventlisner laga ke woh function dale to woh tabhi rukega jab ham time ke andar woh kaam kare.
+
+setInterval(function(){
+    console.log("rajata")
+},3000)
+
+// setInterval function bhi same setTimeout ki tarah hai lekin isme utne second bad phir se function execute hota hai 
