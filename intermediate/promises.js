8e5703ae8ae4e92ca0581b9f1b729797411a3f72
Rajat Singh <126982984+Rajat3254@users.noreply.github.com>
2024-09-27 17:45:54 +0000

    Day : 21

--- intermediate/promises.js
+++ intermediate/promises.js
@@ -10,3 +10,52 @@ new Promise(function(resolve,reject){
 })
 
 // resolve call karene par hi then wale part pe jata hai then tabhi use hota hai jab task resolve ho jata hai
+// industries me to ham settimeout ke andar console log nhi karte hai so resolve jab call hoga to ham usme jobhi pass karenge woh as
+// an argument hame mil jayega through .then
+
+// agar reject hoga to reject(kuch bhi) likh dena hota hai aur .then().catch() se kam hojata hai 
+// .finally () last me pas hota hai hamesha
+
+// hota kya hai ki ham async await ka bhi use kar sakte hai 
+
+const pro=new Promise((resolve,reject)=>{
+   setTimeout( function(){ 
+    let error=false;
+    if(error){
+        resolve('Rajat wowww')
+    }else{
+        reject('rajat aura-1000')
+    }
+},1000)
+})
+
+async function checking(){
+       try {
+        const aaya=await pro
+       console.log(aaya);
+       } catch (error) {
+        console.log(error);
+        
+       }
+       
+}
+checking()
+
+
+// agar ham fetch() pe laga rhe hai async await to
+
+async function getUser(){
+  try {
+    const response= await fetch('reitjietj')
+    const data=await response.json();
+    // ha iske liye bhi await use karte hai
+    console.log(data);
+  } catch (error) {
+    console.log(`error aaagya ${error}`);
+    
+  }
+  
+}
+getUser()
+
+fetch('qriqjr').then().catch().finally()
